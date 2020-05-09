4fe1949d86de79ea062ea0abe79d4a17047444d5
Miguel <migperales30@gmail.com>
2020-05-09 17:35:16 +0200

    Initial commit

---
+++ src/gestion/Main.java
@@ -0,0 +1,19 @@
+package gestion;
+
+public class Main {
+
+    public static void main(String[]args){
+
+    }
+
+    private static void visualizarMenu(){
+        System.out.println("1. Introducir un nuevo gasto.");
+        System.out.println("2. Introducir un nuevo ingreso.");
+        System.out.println("3. Mostrar gastos.");
+        System.out.println("4. Mostrar ingresos.");
+        System.out.println("5. Mostrar el saldo de la cuenta.");
+        System.out.println("0. Salir.");
+
+
+    }
+}
